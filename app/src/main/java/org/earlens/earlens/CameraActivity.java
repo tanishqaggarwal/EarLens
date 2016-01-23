4cbfd739c31fa54276c58b9d5dcfe5e52706574f
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-01-23 08:57:38 -0500

    Initial commit

---
+++ app/src/main/java/org/earlens/earlens/CameraActivity.java
@@ -0,0 +1,56 @@
+package org.earlens.earlens;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.hardware.Camera;
+import android.widget.FrameLayout;
+import android.widget.Button;
+import android.view.View;
+
+import android.content.Intent;
+
+import android.view.Window;
+import android.view.WindowManager;
+
+public class CameraActivity extends Activity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), TypingActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+}
---
+++ app/src/main/java/org/earlens/earlens/TypingActivity.java
@@ -0,0 +1,14 @@
+package org.earlens.earlens;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TypingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_typing);
+    }
+
+}
